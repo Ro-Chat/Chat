92f18b15635f6b8057e7f777537c06fd1b16f9df
Ro-Chat <121181030+Ro-Chat@users.noreply.github.com>
2023-01-13 22:55:50 -0800

    Update main.lua

--- v1/main.lua
+++ v1/main.lua
@@ -1,15 +1,23 @@
-local Request   = syn and syn.request or http and http.request
-local WebSocket = syn and syn.websocket or WebSocket
-local GetAsset  = syn and getsynasset or getcustomasset
+---- Crypt Library
+
+local GCMEncrypt = syn and syn.crypt.encrypt or crypt and function(data, key, nonce) return crypt.custom_encrypt(data, key, nonce, "GCM") end
+local GCMDecrypt = syn and syn.crypt.decrypt or crypt and function(data, key, nonce) return crypt.custom_decrypt(data, key, nonce, "GCM") end
+local decodeb64 = syn and syn.crypt.base64.decode or crypt and crypt.base64decode
+local encodeb64 = syn and syn.crypt.base64.encode or crypt and crypt.base64encode
+
+
+---- Services
 
 local HttpService = game:GetService("HttpService")
 local Players     = game:GetService("Players")
 
-local GCMEncrypt = syn and syn.crypt.encrypt or crypt and function(data, key, nonce) return crypt.custom_encrypt(data, key, nonce, "GCM") end
-local GCMDecrypt = syn and syn.crypt.decrypt or crypt and function(data, key, nonce) return crypt.custom_decrypt(data, key, nonce, "GCM") end
+---- Misc.
 
-local decodeb64 = syn and syn.crypt.base64.decode or crypt and crypt.base64decode
-local encodeb64 = syn and syn.crypt.base64.encode or crypt and crypt.base64encode
+local Request   = syn and syn.request or http and http.request
+local WebSocket = syn and syn.websocket or WebSocket
+local GetAsset  = syn and getsynasset or getcustomasset
+
+---- Imports
 
 local Import  = function(path) return loadstring(game:HttpGet(("https://raw.githubusercontent.com/Ro-Chat/Chat/main/v1/Modules/%s.lua"):format(path))).Body)() end
 local Utility = Import("Utility")
